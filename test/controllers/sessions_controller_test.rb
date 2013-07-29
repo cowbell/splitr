3c69f36b52e32eb3aebec1d537291e6f02638ac5
Kuba Kuźma <kuba@jah.pl>
2013-07-29 11:24:39 +0200

    added controller tests #2

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionController::TestCase
+  test "new renders template when no user logged in" do
+    get :new
+    assert_template "new"
+  end
+
+  test "new raises exception when user logged in" do
+    session[:user_id] = create(:user).id
+    assert_raises(CanCan::AccessDenied) { get :new }
+  end
+
+  test "create redirects to root, sets session when valid params given" do
+    user = create(:user)
+    post :create, session: {email: user.email, password: user.password}
+    assert_response :redirect
+    assert session[:user_id].present?
+  end
+
+  test "create raises exception when user logged in" do
+    session[:user_id] = create(:user).id
+    assert_raises(CanCan::AccessDenied) { post :create, session: {email: "user@example.com", password: "secret"} }
+  end
+
+  test "destroy redirects to root and unsets session" do
+    session[:user_id] = create(:user).id
+    delete :destroy
+    assert_response :redirect
+    assert session[:user_id].blank?
+  end
+
+  test "destroy raises exception when no user logged in" do
+    assert_raises(CanCan::AccessDenied) { delete :destroy }
+  end
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 class UsersControllerTest < ActionController::TestCase
-  test "new renders template" do
+  test "new renders template when no user logged in" do
     get :new
     assert_template "new"
+  end
+
+  test "new raises exception when user logged in" do
+    session[:user_id] = create(:user).id
+    assert_raises(CanCan::AccessDenied) { get :new }
   end
 
   test "create redirects to root, creates user, sets session when valid params given" do
@@ -11,5 +16,11 @@ class UsersControllerTest < ActionController::TestCase
     post :create, user: user_attributes
     assert_response :redirect
     assert User.exists?(email: user_attributes[:email])
+    assert session[:user_id].present?
+  end
+
+  test "create raises exception when user logged in" do
+    session[:user_id] = create(:user).id
+    assert_raises(CanCan::AccessDenied) { post :create, user: attributes_for(:user) }
   end
 end
