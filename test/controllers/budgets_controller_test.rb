feac84956378998036bd43b1a68285633dffbc41
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-07-31 17:09:55 +0200

    user can add members to budget. Closes #4

--- test/controllers/budgets_controller_test.rb
+++ test/controllers/budgets_controller_test.rb
@@ -23,6 +23,25 @@ class BudgetsControllerTest < ActionController::TestCase
     assert_equal [budget], assigns(:budgets)
   end
 
+  # show
+  test "show raises exception when logged in user does not belong to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    session[:user_id] = user.id
+
+    assert_raises(ActiveRecord::RecordNotFound) { get :show, id: budget.id }
+  end
+
+  test "show renders template when user logged in and belongs to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    create(:member, budget: budget, user: user)
+    session[:user_id] = user.id
+    get :show, id: budget.id
+
+    assert_template :show
+  end
+
   # new
   test "new raises exception when no user logged in" do
     assert_raises(CanCan::AccessDenied) { get :new }
@@ -39,11 +58,11 @@ class BudgetsControllerTest < ActionController::TestCase
     assert_raises(CanCan::AccessDenied) { post :create, budget: attributes_for(:budget) }
   end
 
-  test "create redirects to root, creates budget when user logged in" do
+  test "create redirects to budgets, creates budget when user logged in" do
     session[:user_id] = create(:user).id
     budget_attributes = attributes_for(:budget)
     post :create, budget: budget_attributes
-    assert_response :redirect
+    assert_redirected_to budgets_path
     assert Budget.find_by(name: budget_attributes[:name]).present?
   end
 
