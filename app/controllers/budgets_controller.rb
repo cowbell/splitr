3849f26bd627b414f4fccb7b14696eaeeb5a412c
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-08-05 17:15:42 +0200

    added editing and destroying of budget. Closes #20

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -24,6 +24,28 @@ class BudgetsController < ApplicationController
     @budget = current_user.budgets.find(params[:id])
   end
 
+  def edit
+    authorize! :update, Budget
+    @budget = current_user.budgets.find(params[:id])
+  end
+
+  def update
+    authorize! :update, Budget
+    @budget = current_user.budgets.find(params[:id])
+    if @budget.update(budget_params)
+      redirect_to budgets_path, notice: "Budget has been successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize! :destroy, Budget
+    @budget = current_user.budgets.find(params[:id])
+    @budget.destroy
+    redirect_to budgets_path, notice: "Budget has been successfully removed."
+  end
+
   private
 
   def budget_params
--- test/controllers/budgets_controller_test.rb
+++ test/controllers/budgets_controller_test.rb
@@ -73,4 +73,85 @@ class BudgetsControllerTest < ActionController::TestCase
 
     assert Member.where(user_id: user.id, name: "John Doe").exists?
   end
+
+  # edit
+  test "edit raises exception when no logged in user" do
+    budget = create(:budget)
+    assert_raises(CanCan::AccessDenied) { get :edit, id: budget.id }
+  end
+
+  test "edit raises exception when logged in user does not belong to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    session[:user_id] = user.id
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get :edit, id: budget.id
+    end
+  end
+
+  test "edit renders template when logged in user belongs to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    create(:member, budget: budget, user: user)
+    session[:user_id] = user.id
+    get :edit, id: budget
+
+    assert_template :edit
+  end
+
+  # update
+  test "update raises exception when no logged in user" do
+    budget = create(:budget)
+    budget_params = attributes_for(:budget)
+    assert_raises(CanCan::AccessDenied) do
+      put :update, id: budget.id, budget: budget_params
+    end
+  end
+
+  test "update raises exception when logged in user does not belong to budget" do
+    user = create(:user)
+    session[:user_id] = user.id
+    budget = create(:budget)
+    budget_params = attributes_for(:budget)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      put :update, id: budget.id, budget: budget_params
+    end
+  end
+
+  test "update redirects to budgets and updates budget when logged in user belongs to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    create(:member, budget: budget, user: user)
+    session[:user_id] = user.id
+    budget_params = attributes_for(:budget, name: "Updated name")
+    put :update, id: budget.id, budget: budget_params
+
+    assert_redirected_to budgets_path
+    assert_equal "Updated name", budget.reload.name
+  end
+
+  # destroy
+  test "destroy raises exception when no logged in user" do
+    budget = create(:budget)
+    assert_raises(CanCan::AccessDenied) { delete :destroy, id: budget.id }
+  end
+
+  test "destroy raises exception when logged in user does not belong to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    session[:user_id] = user.id
+    assert_raises(ActiveRecord::RecordNotFound) do
+      delete :destroy, id: budget.id
+    end
+  end
+
+  test "destroy redirects to budgets when logged in user belongs to budget" do
+    user = create(:user)
+    budget = create(:budget)
+    create(:member, budget: budget, user: user)
+    session[:user_id] = user.id
+    delete :destroy, id: budget.id
+
+    assert_redirected_to budgets_path
+  end
 end
