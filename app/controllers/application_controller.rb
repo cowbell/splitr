db8d17d673c3c8b29425c1d418dfd9c208dec539
Szymon Nowak <szimek@gmail.com>
2013-08-07 13:36:42 +0200

    Handle CanCan::AccessDenied exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include CurrentUser
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
 end
