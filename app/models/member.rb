42e0ab659acde36268873a9bc2329e3a691aa898
Kuba Kuźma <kuba@jah.pl>
2013-09-14 19:04:13 +0200

    added other side of the payer association

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ActiveRecord::Base
   belongs_to :budget
   has_many :participations, inverse_of: :member, dependent: :destroy
   has_many :transactions, through: :participations, source: :transaction
+  has_many :transactions, inverse_of: :payer, foreign_key: :payer_id, dependent: :nullify
 
   validates :name,   presence: true, uniqueness: {scope: :budget_id}
   validates :budget, presence: true
